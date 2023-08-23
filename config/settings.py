959528a2ae165ea25cf57d5bc070701e149ab2dd
vividruby <bluetiful@naver.com>
2023-08-23 17:04:04 +0900

    models

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    # 'restaurant_app.apps.Restaurant_appConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
